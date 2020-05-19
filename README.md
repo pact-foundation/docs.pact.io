bffa22e737edf72bf256ad833531ae1e093d6ae6
Beth Skurrie <beth@bethesque.com>
2020-05-20 07:58:49 +1000

    chore: update site config

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pact Docs Website
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/17da9563-50ff-43b0-b0b7-d4570e40a849/deploy-status)](https://app.netlify.com/sites/inspiring-panini-26eb5e/deploys)
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  title: 'Pact', // Title for your website.
+  tagline: 'Fast and reliable testing for your APIs and microservices during development. Safety during deployment.',
+  url: 'https://inspiring-panini-26eb5e.netlify.app', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'pact-docs',
+  organizationName: 'Pact Foundation',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -73,7 +73,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Pact Foundation`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
@@ -94,17 +94,17 @@ const siteConfig = {
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
-  // docsSideNavCollapsible: true,
+  docsSideNavCollapsible: true,
 
   // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
+  enableUpdateBy: true,
 
   // Show documentation's last update time.
-  // enableUpdateTime: true,
+  enableUpdateTime: true,
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+    repoUrl: 'https://github.com/pact-foundation/docs.pact.io',
 };
 
 module.exports = siteConfig;
