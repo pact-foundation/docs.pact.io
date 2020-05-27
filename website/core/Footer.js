3f3fd7b6a58fcf496c04bd0e0c6c0df47812041c
Beth Skurrie <beth@bethesque.com>
2020-05-28 08:57:42 +1000

    fix: links, move how-pact-works.html

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -52,7 +52,7 @@ class Footer extends React.Component {
           </div>
           <div>
             <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
+            <a href={this.pageUrl('users', this.props.language)}>
               User Showcase
             </a>
             <a
