21c609c832653f6737e2cddef06e0fad544dbc20
Beth Skurrie <beth@bethesque.com>
2020-06-25 13:59:24 +1000

    chore: add script to trigger workflow

---
+++ scripts/trigger-sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -X POST https://api.github.com/repos/pact-foundation/docs.pact.io/dispatches \
+      -H 'Accept: application/vnd.github.everest-preview+json' \
+      -H "Authorization: Bearer $GITHUB_ACCESS_TOKEN" \
+      -d '{"event_type": "pact-js-docs-updated"}'
+
+echo "See https://github.com/pact-foundation/docs.pact.io/actions?query=workflow%3A%22Sync+Pact+docs%22"
