36581e8258808aef04c21e4cf453e72ff0e9bbde
Beth Skurrie <beth@bethesque.com>
2020-08-15 15:17:03 +1000

    chore: update sync rust script to escape < and >

--- scripts/sync/pact_rust.rb
+++ scripts/sync/pact_rust.rb
@@ -3,8 +3,15 @@ require_relative 'support'
 
 SOURCE_REPO = 'pact-foundation/pact-reference'
 DESTINATION_DIR = relative_path_to('docs/implementation_guides/rust')
-TRANSFORM_PATH = -> (path) { File.join(DESTINATION_DIR, path.downcase.delete_prefix('rust').gsub('/readme.md', '/index.md')) }
+TRANSFORM_PATH = -> (path) {
+  if path == 'rust/README.md'
+    "#{DESTINATION_DIR}.md"
+  else
+    File.join(DESTINATION_DIR, path.downcase.delete_prefix('rust/').gsub('/readme.md', '.md'))
+  end
+}
 INCLUDE = [
+  # ->(path) { path.end_with?('pact_mock_server_cli/README.md') }
   ->(path) { path.start_with?('rust') && path.end_with?('.md') }
 ]
 IGNORE = []
@@ -12,15 +19,11 @@ CUSTOM_ACTIONS = [
   [->(path) { !path.end_with?('CHANGELOG.md') }, ->(md_file_contents) { md_file_contents.extract_title } ],
   [->(path) { path.end_with?('CHANGELOG.md') }, ->(md_file_contents) {
     md_file_contents.fields[:title] = md_file_contents.fields[:custom_edit_url].split('/')[-2]
-    md_file_contents.remove_lines_including('To generate the log')
-    md_file_contents.remove_lines_including('- chore: ')
-    md_file_contents.remove_lines_including('- docs: ')
-    md_file_contents.remove_lines_including('- doc: ')
-    md_file_contents.remove_lines_including('- refactor: ')
-    md_file_contents.remove_lines_including('- style: ')
+    md_file_contents.clean_up_changelog
     md_file_contents.find_and_replace(/^# /, '## ')
   }],
   ["rust/README.md", ->(md_file_contents) { md_file_contents.fields[:title] = "Overview" } ],
+  [:all, ->(md_file_contents) { md_file_contents.escape_things_that_look_like_jsx_tags }]
 ]
 
 
--- scripts/sync/support.rb
+++ scripts/sync/support.rb
@@ -93,6 +93,33 @@ class MarkdownFileContents
     @lines = lines.collect { | line | UrlAbsolutizer.absolutize_links(line, repository_slug, path_transformer, contents_source_path, source_paths) }
   end
 
+  def escape_things_that_look_like_jsx_tags
+    in_code_block = false
+    @lines = lines.collect do | line |
+      in_code_block = !in_code_block if line.start_with?('```')
+
+      if !in_code_block
+        in_backticks = false
+        line.chars.collect do | char |
+          # Doesn't handle escaped backticks within backticks
+          in_backticks = !in_backticks if char == '`'
+
+          if !in_backticks
+            case char
+            when '<' then '&lt;'
+            when '>' then '&;gt;'
+            else char
+            end
+          else
+            char
+          end
+        end.join
+      else
+        line
+      end
+    end
+  end
+
   private
 
   def header_lines
