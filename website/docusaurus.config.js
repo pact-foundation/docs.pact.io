ba49939b0bfe83ea771a3972ca94c1d3c58a7981
Matt Fellows <matt.fellows@onegeek.com.au>
2020-11-06 14:41:20 +1100

    docs: link to pactflow docs

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -151,6 +151,7 @@ const siteConfig = {
         {to: 'pact_broker/docker_images', label: 'Pact Broker Docs', position: 'left'},
         {to: 'history', label: 'Resources', position: 'left'},
         {to: 'help', label: 'Help', position: 'left'},
+        {to: 'https://docs.pactflow.io', label: 'Pactflow Docs ↗️', position: 'left'},
         {to: "https://go.pactflow.io/nov20meetup?utm_source=docs.pact.io&utm_medium=web&utm_campaign=meetup-nov-2020&utm_content=banner", label: ' 👋 Join our global community event on Nov 19', position: 'right'},
       ],
     },
